5a9838a228e56d3e2d04aeb853c8546bbdf1a3ab
nealcaffre <31470135+nealcaffre@users.noreply.github.com>
2022-06-05 02:08:13 +0530

    chnages for development

--- src/component/Input/Input.jsx
+++
@@ -1,9 +0,0 @@
-import input from "./Input.module.css";
-
-export const Input = ({type='text',onChangeHandler=()=>{},onKeyPressHandler=()=>{}}) => {
-    return (
-        <>
-            <input type={type} onKeyDown={onKeyPressHandler} className={`${input.border} ${input.width} ${input.font__size}`} onChange={onChangeHandler} />
-        </>
-    )
-}
